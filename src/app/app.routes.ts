2872fe4bbd2ee1a1fc7258016f2fd8818433f9b3
Anatolii Stoliarenko <anatolii.stoliarenko@gmail.com>
2024-07-14 02:42:44 +0200

    calendar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/register', pathMatch: 'full' }, // Default route
+  { path: 'calendar', component: CalendarComponent },
+  { path: '', redirectTo: '/calendar', pathMatch: 'full' }, // Default route
 ];
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { CommonModule } from '@angular/common';
+import { HourCalendarComponent } from "./hour/hour.component";
 
 @Component({
   selector: 'app-calendar',
-  standalone: true,
-  imports: [],
   templateUrl: './calendar.component.html',
-  styleUrl: './calendar.component.scss'
+  styleUrls: ['./calendar.component.scss'],
+  standalone: true,
+  providers: [provideNativeDateAdapter()],
+  imports: [MatCardModule, MatDatepickerModule, CommonModule, HourCalendarComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CalendarComponent {
+  selectedDates: Date[] = [];
+  selectedDate: Date | null = null;
+  minDate: Date = new Date();
+  maxDate: Date = new Date();
+  selectedHours: { [key: string]: string[] } = {};
 
+  reservedDates: Date[] = [new Date('2024-07-15'), new Date('2024-07-25')];
+  availableDates: Date[] = [new Date('2024-07-16'), new Date('2024-07-18')];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.initMaxDate();
+  }
+
+  initMaxDate() {
+    this.maxDate.setFullYear(this.maxDate.getFullYear() + 1);
+  }
+
+  formatDate(date: Date): string {
+    return date.toISOString().split('T')[0];
+  }
+
+  dateClass = (date: Date) => {
+    const dateString = date.toDateString();
+    if (
+      this.selectedHours[dateString] &&
+      this.selectedHours[dateString].length > 0
+    ) {
+      return 'reserved-date';
+    } else {
+      return 'available-date';
+    }
+  };
+
+  updateSelectedHours(dateString: string, hours: string[]) {
+    this.selectedHours[dateString] = hours;
+  }
+
+  select(date: Date | null) {
+    if (!date) return;
+
+    this.selectedDate = date;
+    this.selectedDates.push(this.selectedDate);
+    console.log(this.formatDate(this.selectedDate));
+  }
 }
