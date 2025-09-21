3f57d7c009d50b93e86ecbe390534a608aa44266
Anatolii Stoliarenko <anatolii.stoliarenko@gmail.com>
2025-09-22 01:20:09 +0200

    fix/themes fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,20 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { AuthService } from './auth/services/auth.service';
-import { ThemeService } from './core/services/theme.service';
+// import { ThemeService } from './core/services/theme.service';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  private readonly _authService = inject(AuthService);
-  private readonly _themeService = inject(ThemeService);
+    private readonly _authService = inject(AuthService);
+    // private readonly _themeService = inject(ThemeService);
 
-  ngOnInit(): void {
-    this._authService.restoreUser();
-  }
+    ngOnInit(): void {
+        this._authService.restoreUser();
+    }
 }
